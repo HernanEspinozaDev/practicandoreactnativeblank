eeb119384cb694f4e39346fc9cc2a7b671f897c1
HernanEspinozaDev <hernan.espinoza.dev@gmail.com>
2024-08-13 10:16:07 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={icon} style={{ width: 100, height: 100 }} />
+      <Text style ={{ color: 'white'}}>Probando hacer esto en android</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
